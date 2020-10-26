8b0134c80dcf21d54e28546277ee20da5be24ed0
Yehuda Bortz <yehudabortz@icloud.com>
2020-10-26 11:46:50 -0500

    commit tree 968c64c4bae38900a41b4e380a3dd457f98b64d5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |n|
+    badges.push("Hello, my name is #{n}.")
+    end
+    badges
+end
+
+def assign_rooms(attendees)
+    room_assignments =[]
+    attendees.each_with_index do |name, index|
+        index += 1
+       room_assignments.push "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    room_assignments
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |n|
+        # binding.pry
+        puts n
+    end
+    assign_rooms(attendees).each do |room|
+        puts room
+    end
+end
